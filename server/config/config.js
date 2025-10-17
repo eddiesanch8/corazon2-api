59b96bb5ab006dfeb5ff2b6bc26d27af19ded331
eddiesanch8 <146777616+eddiesanch8@users.noreply.github.com>
2025-10-16 21:28:41 -0400

    Update database configuration for MySQL

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 const dbConfig = {
-  host: process.env.HOST,
-  user: process.env.DB_USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  database: process.env.MYSQLDATABASE,
+  password: process.env.MYSQLPASSWORD,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 };
 
 export default dbConfig;
